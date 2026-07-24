ac5160ac8078109629b64ff8bde9dafea361f47f
nandakshat9988 <nand.akshat2004@gmail.com>
2026-07-24 18:59:12 +0530

    added docker

--- README.d
+++
@@ -1,7 +0,0 @@
-# 📈 StockVision AI
-
-StockVision AI is a web application that predicts the next day's stock closing price using Machine Learning.
-Users can search for a stock, view company information, analyze historical price data, and receive a predicted closing price along with a simple
-Buy, Hold, or Sell recommendation. The frontend is built with HTML, CSS, and JavaScript, while the backend uses Flask and Scikit-learn for prediction.
-Stock data is fetched using the Finnhub API and Yahoo Finance. This project was developed to understand how machine learning models can be
-integrated with web applications and to gain hands-on experience with full-stack development.
